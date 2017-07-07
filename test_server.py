caa119b27181bbfd9e4d7dc1e64f19d9ea16ab55
Avraham <avraham.shukron@gmail.com>
2017-07-07 18:03:55 +0300

    PEP8: Convert tabs to spaces
    Bad bad editor!

--- test_server.py
+++ test_server.py
@@ -2,27 +2,26 @@ import sys
 import socket
 
 
-
 def main(port):
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	s.bind(("0.0.0.0", port))
-	s.listen(1)
-	while True:
-		print "Waiting for client..."
-		client, addr = s.accept()
-		print "Client connected at %s" % (addr,)
-		while True:
-			try:
-				data = client.recv(1024)
-				print data
-			except Exception as e:
-				print str(e)
-				client.close()
-				print "Client closed"
-				break
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(("0.0.0.0", port))
+    s.listen(1)
+    while True:
+        print "Waiting for client..."
+        client, addr = s.accept()
+        print "Client connected at %s" % (addr,)
+        while True:
+            try:
+                data = client.recv(1024)
+                print data
+            except Exception as e:
+                print str(e)
+                client.close()
+                print "Client closed"
+                break
 
 
 if __name__ == '__main__':
-	port = int(sys.argv[1])
-	main(port)
+    port = int(sys.argv[1])
+    main(port)
